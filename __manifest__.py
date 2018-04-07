f343b38f18af9a2471e74c697a4563c73840408a
Christian <rakoto.christall@gmail.com>
2018-04-07 11:34:48 +0300

    Initialisation du fichier manifest

---
+++ __manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Interface utilisateur pour améliorer l\'application todo',
+    'description': 'Module utilisateur simple',
+    'author': 'Rakotomalala Christian',
+    'depends': ['todo_user'] 
+}
